cc96420e182a170dc1372b930eb01b96f76be837
2m8 <doyun7731@gmail.com>
2025-10-05 13:57:35 +0900

    resourceManager 수정

--- src/Resource/ResourceManager.js
+++ src/Resource/ResourceManager.js
@@ -11,6 +11,28 @@ class ResourceManager {
 
         this.loaded = true;
     }
+
+    #getJsonData(fileName) {
+        const baseDir = join(process.cwd(), 'AlkkagiData');
+
+        const path = await import("node:path");
+        const fs = await import("node:fs/promises");
+        const fss = await import("node:fs");
+
+        const p = path.resolve(baseDir, fileName);
+        if (!fss.existsSync(p)) {
+            console.log(`[${ctor.name}] File not found: ${p}`);
+            return;
+        }
+
+        let text;
+        try {
+            text = await fs.readFile(p, "utf8");
+        } catch (e) {
+            console.log(`[${ctor.name}] File read error: ${p}`);
+            return;
+        }
+    }
 }
 
 export { ResourceManager };
