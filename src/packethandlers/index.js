def859cb429f05d960a0d7eab974d06d5a05ae37
SEH00N <06psh0501@gmail.com>
2025-10-06 20:15:41 +0900

    [ETC] Dir/File Renaming - pre

--- src/packethandlers/index.js
+++
@@ -1,3 +0,0 @@
-export { ServerPacketHandler } from './Base/ServerPacketHandler.js';
-export { MessagePacketHandler } from './MessagePacketHandler.js';
-export { VectorPacketHandler } from './VectorPacketHandler.js';
--- src/packethandlers/vectorpackethandler.js
+++
@@ -1,9 +0,0 @@
-import { ServerPacketHandler } from './index.js';
-
-class VectorPacketHandler extends ServerPacketHandler {
-    handle(packet) {
-        globalThis.logger.info('VectorPacketHandler', `Received vector: ${packet.vector.x}, ${packet.vector.y}`);
-    }
-}
-
-export { VectorPacketHandler };
