06c5ea1638059eadf2c549234f0926ec4a88db82
SEH00N <06psh0501@gmail.com>
2025-10-14 01:45:56 +0900

    [Entity] Folder Hierarchy Refactoring

--- src/Entity/index.js
+++ src/Entity/index.js
@@ -1,7 +1,7 @@
+export { Entity } from './Entity.js'
+export { Unit } from './Unit.js'
 export { Character } from './Character/Character.js';
 export { Player } from './Player.js'
 export { BotPlayer } from './BotPlayer.js'
-export { Unit } from './Unit.js'
 export { XPObject } from './XPObject.js'
 export { XPContainer } from './XPContainer.js'
-export { Entity } from './Entity.js'
