e987ee440d3e922dd4910d93e16d1f261722bbea
SEH00N <06psh0501@gmail.com>
2025-10-27 00:34:33 +0900

    [Network] Handle Client Disconnect

--- src/Core/PlayerHandle.js
+++ src/Core/PlayerHandle.js
@@ -4,6 +4,13 @@ class PlayerHandle {
         this.world = world;
         this.clientHandle = clientHandle;
         this.playerEntity = playerEntity;
+
+        this.clientHandle.on('disconnect', this.handleClientDisconnect.bind(this));
+    }
+
+    handleClientDisconnect() {
+        globalThis.logger.info('GameServer', 'Player entity removed by client disconnect');
+        this.world.removeEntity(this.playerEntity);
     }
 }
 
